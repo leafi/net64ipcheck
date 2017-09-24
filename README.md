8fb61d48001f97b68fd28fc59c95525293372cce
leaf <leaf@leaf.sx>
2017-09-24 16:18:07 +0100

    Simplify & fix port checking service

--- README.md
+++ README.md
@@ -10,25 +10,29 @@ Server: Run the binary. It listens on port :6460 UDP.
 
 Consider a user attempting to start a server on port 8000. Their IP is 1.2.3.4.
 
-aaaa, bbbb etc. are dynamically chosen/irrelevant ports.
-
-**PAY ATTENTION TO THE PORT DANCE.**
-
 The first UDP packet sent to api.?????.com:6460 must be 400 bytes.
 
 It must contain the UTF-8 string `net64ipc0000`, followed by 2 bytes encoding the server port (typically 8000), and then the remaining 386 bytes can be anything.
 
+The packet must be sent from the server port encoded in the message.
+
 **Wrong version scenario**
 
-1.2.3.4:aaaa -> api.?????.com:6460   (sends garbage that's 400 bytes long)
+1.2.3.4:8080 -> api.?????.com:6460   (sends garbage that's 400 bytes long)
 
-api.?????.com:bbbb -> 1.2.3.4:aaaa   `BADVER`
+api.?????.com:6460 -> 1.2.3.4:8080   `BADVER`
+
+**Wrong port scenario**
+
+1.2.3.4:8123 -> api.?????.com:6460   `net64ipc0000(binary short representing 5678)`
+
+api.?????.com refuses to respond & ignores the request.
 
 **Correct version but port closed**
 
-1.2.3.4:aaaa -> api.?????.com:6460    `net64ipc0000(binary short representing 8000)`
+1.2.3.4:8080 -> api.?????.com:6460    `net64ipc0000(binary short representing 8000)`
 
-api.?????.com:bbbb -> 1.2.3.4:8000    `TEST`
+api.?????.com:6460 -> 1.2.3.4:8000    `OK 1.2.3.4`
 
 (... packet never gets through to 1.2.3.4:8000 ...)
 
@@ -38,18 +42,12 @@ The client should consider sending 3 attempts 500ms apart for every "test" to de
 
 **Correct version and port open**
 
-1.2.3.4:aaaa -> api.?????.com:6460    `net64ipc0000(binary short representing 8000)`
+1.2.3.4:8080 -> api.?????.com:6460    `net64ipc0000(binary short representing 8000)`
 
-api.?????.com:bbbb -> 1.2.3.4:8000    `TEST`
-
-1.2.3.4:8000 -> api.?????.com:bbbb    `TOAST000`
-
-api.?????.com:cccc -> 1.2.3.4:aaaa    `OK 1.2.3.4`
+api.?????.com:6460 -> 1.2.3.4:8080    `OK 1.2.3.4`
 
 Session ends.
 
 As UDP is lossy, the client should rely on timeout to detect failure but should probably send out 3 requests spaced 500ms apart or so & if any OK reply comes back then consider the port open.
-
-1.2.3.4:8000 must send back TOAST000 within **2 seconds**.
 
 (If 3 (or 5, whatever) attempts aren't enough or packet takes >2s, the user's connection is so shit we may as well say "hey, this ain't gonna work".)
--- main.go
+++ main.go
@@ -4,8 +4,12 @@ import (
 	"encoding/binary"
 	"fmt"
 	"net"
-	"time"
 )
+
+func prepend(xs []byte) []byte {
+	var xb1 = append([]byte{}, 0x43, 0x4f, 0x4e, 0x43)
+	return append(xb1, xs...)
+}
 
 func main() {
 	fmt.Println("#######\nStarting net64ipcheck.\n#######")
@@ -36,55 +40,27 @@ func main() {
 			// packet must be 400 bytes; larger than necessary to prevent attacks where Outgoing >>> Incoming
 			// (i.e. using us to spam the ostensible sender with more data)
 			fmt.Println("Bad fmt packet from ", raddr, " (not 400 bytes)")
-		} else if string(buf[0:11]) != "net64ipc0000" {
+		} else if string(buf[0:12]) != "net64ipc0000" {
 			fmt.Println("Bad fmt packet from ", raddr, " (expected net64ipc0000)")
 			go func(raddr *net.UDPAddr) {
-				c, err := net.DialUDP("udp", nil, raddr)
-				defer c.Close()
-
-				if err == nil {
-					c.Write([]byte("BADVER"))
-				}
+				ln.WriteToUDP(prepend([]byte("BADVER")), raddr)
 			}(raddr)
 		} else {
 			// parse server port from packet
-			srvport := binary.LittleEndian.Uint16(buf[12:13])
-			go func(raddr *net.UDPAddr, srvport uint16) {
-				srvAddr := &net.UDPAddr{
-					IP:   raddr.IP,
-					Port: int(srvport),
-					Zone: raddr.Zone,
-				}
+			srvport := binary.LittleEndian.Uint16(buf[12:14])
 
-				c, err := net.DialUDP("udp", nil, srvAddr)
-				defer c.Close()
-
-				if err != nil {
-					fmt.Println("Failed to construct udp address for given port ", srvport, " (raddr ", raddr, " srvAddr ", srvAddr, ")")
-				} else {
-					c.Write([]byte("TEST"))
-
-					c.SetReadDeadline(time.Now().Add(time.Second * 2))
-
-					rbuf := make([]byte, 9)
-					n, err := c.Read(rbuf)
+			// only allow srvport == raddr.Port (Net64 tool does this, and it makes things simpler.)
+			if int(srvport) == raddr.Port {
+				go func(raddr *net.UDPAddr, srvport uint16) {
+					_, err := ln.WriteToUDP(prepend([]byte("OK "+raddr.IP.String())), raddr)
 
 					if err != nil {
-						fmt.Println("  no/err read from sending TEST to ", srvAddr, " (raddr ", raddr, ")")
-					} else if n != 8 {
-						fmt.Println("  got response but a bad one from sending TEST to ", srvAddr, " (expected 8 bytes)")
-					} else if string(rbuf[0:7]) != "TOAST000" {
-						fmt.Println("  got response but bad one from sending TEST to ", srvAddr, " (expected TOAST000)")
-					} else {
-						c2, err := net.DialUDP("udp", nil, raddr)
-						defer c2.Close()
-
-						if err != nil {
-							c2.Write([]byte("OK " + srvAddr.IP.String()))
-						}
+						fmt.Println(err, "\n^ Failed to write OK for given port ", srvport, " (raddr ", raddr, ")")
 					}
-				}
-			}(raddr, srvport)
+				}(raddr, srvport)
+			} else {
+				fmt.Println("srvport != raddr.Port; ignoring message!")
+			}
 		}
 	}
 
